51cd63afd0ace2063157d7f4f397eed276fa0e7e
mustafaobaidd <dodor949@gmail.com>
2024-07-02 21:38:26 +0300

    else

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ app.get("/", (req, res) => {
     const day = today.getDay();
 
     // console.log(day);
-    let type = "a weekday";
-    let adv = "it's time to work hard";
+    let type;
+    let adv;
 
     if (day === 0 || day === 6){
         type = "the weekend";
         adv = "it's time have some fun";
+    }
+    else{
+        type = "a weekday";
+        adv = "it's time to work hard";
     }
 
     res.render("index.ejs", {
